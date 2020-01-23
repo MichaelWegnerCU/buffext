0781b8da1866b7221227f6601f8f9d120d154e45
unknown <miwe7500@colorado.edu>
2020-01-23 13:34:03 -0700

    Come on work

---
+++ content.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+}
