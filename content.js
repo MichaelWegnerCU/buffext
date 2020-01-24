9bbb4906cb58f4b8e8526ae4fe2714aa54309e50
unknown <miwe7500@colorado.edu>
2020-01-23 18:02:45 -0700

    Am now able to get classes

--- content.js
+++ content.js
@@ -1,8 +1,17 @@
 // JavaScript Document
 
+
+
 function ready() {
 	console.log("Here!");
 	var top5 = document.getElementsByTagName("app-holds-todos-card")[0];
+	var logo = document.getElementsByClassName("responsiveBuff")[0];
+	var courses= document.getElementsByClassName("schedule-clz");
+	console.log(courses[1].outerHTML);
+	logo.src="https://github.com/MichaelWegnerCU/buffext/blob/master/newlogo.png?raw=true";
+	logo.height =250;
+	logo.width = 400;
+	
 	//var column_0 =document.getElementById('column-0');
 	top5.innerHTML = `
 	<div class=top5>
@@ -11,16 +20,16 @@ function ready() {
 			<div class="newheader_text">TOP 3</div>
 		</div>
 		<div class="t5_list">
-			<ul style="list-style-type:none;">
-  				<li><span _ngcontent-fde-c6="" class="summary-icon mat-icon fal fa-calendar-check" mat-card-avatar=""></span> <a href="https://buffportal.colorado.edu/card/Buff%20Portal%20Advising" class="t5_link">Advising</a></li>
-  				<li><span _ngcontent-xdi-c6="" class="summary-icon mat-icon fal fa-graduation-cap" mat-card-avatar=""></span><a href="https://buffportal.colorado.edu/card/Degree%20Audit" class="t5_link" >Degree Audit</a></li>
-  				<li><span _ngcontent-xdi-c6="" class="summary-icon mat-icon fal fa-pencil-alt" mat-card-avatar=""></span><a href="https://buffportal.colorado.edu/card/CHANGE%20YOUR%20SCHEDULE" class="t5_link">Change schedule (Add Drop)</a></li>
+			<ul >
+  				<li><span _ngcontent-fde-c6="" class="summary-icon mat-icon size_control fa-calendar-check" mat-card-avatar=""></span> <a href="https://buffportal.colorado.edu/card/Buff%20Portal%20Advising" class="t5_link">Advising</a></li>
+  				<li><span _ngcontent-xdi-c6="" class="summary-icon mat-icon size_control fa-graduation-cap" mat-card-avatar=""></span><a href="https://buffportal.colorado.edu/card/Degree%20Audit" class="t5_link" >Degree Audit</a></li>
+  				<li><span _ngcontent-xdi-c6="" class="summary-icon mat-icon size_control fa-pencil-alt" mat-card-avatar=""></span><a href="https://buffportal.colorado.edu/card/CHANGE%20YOUR%20SCHEDULE" class="t5_link">Change schedule (Add Drop)</a></li>
 			</ul>
 		</div>
 	</div> 
 	`;
- 
+ 	console.log(courses);
   }
 
-setTimeout(ready, 200);
+setTimeout(ready, 800);
 
